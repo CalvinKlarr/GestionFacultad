107e82a5ffd14c84ed8ed772711cf5ac23876d19
Juan Andres Calvo <juan_andres_calvo@yahoo.com.ar>
2019-06-08 10:56:34 -0300

    Agregar archivos de proyecto.

---
+++ GestionFacultad/Aula.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFacultad
+{
+    class Aula
+    {
+        private int id;
+        public int Id { get { return id; } set { id = value; } }
+        private int capacidad;
+        public int Capacidad { get { return capacidad; } set { capacidad = value; } }
+        private bool conexionARed;
+        public bool ConexionARed { get { return conexionARed; } set { conexionARed = value; } }
+        private bool proyeccion;
+        public bool Proyeccion { get { return proyeccion; } set { proyeccion = value; } }
+
+        public Aula(int i, int cap, bool con, bool proy)
+        {
+            this.id = i;
+            this.capacidad = cap;
+            this.conexionARed = con;
+            this.proyeccion = proy;
+
+
+        }
+
+
+
+    }
+}
---
+++ GestionFacultad/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFacultad
+{
+    abstract class Usuario
+    {
+        protected string nombre;
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+
+        protected string apellido;
+        public string Apellido { get { return apellido; } set { apellido = value; } }
+
+        protected int dni;
+        public int Dni { get { return dni; } set { dni = value; } }
+
+        protected int tel;
+        public int Tel { get { return tel; } set { tel = value; } }
+
+        protected string direc;
+        public string Direc { get { return direc; } set { direc = value; } }
+    }
+}
